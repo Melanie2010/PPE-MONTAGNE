621321ce741f4b5c61417b6ea49ccec7dfd670ee
Melanie2010 <65156750+Melanie2010@users.noreply.github.com>
2021-05-30 15:02:23 +0200

    Add files via upload

---
+++ src/header.inc.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vacances à la montagne</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <header>
+        <h1>Vacances à la montagne</h1>
+        <nav>
+            <ul>
+                <li><img src="./asset/logo.png" alt="logo"></li>
+                <li><a href="#">Guide des stations</a></li>
+                <li><a href="#guide">Magazine</a></li>
+                <li><a href="#">Info live</a></li>
+                <li><a href="#reservation">Réservation</a></li>
+                <li><a href="#"><img src="./asset/loop.png" alt="recherche"></a></li>
+            </ul>
+        </nav>
+    </header>
